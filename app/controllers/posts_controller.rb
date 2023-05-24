adc3059915e054f4228fffe7fbd34d590677ae15
yuta <calendula6724yuta@gmail.com>
2023-05-24 22:51:29 +0900

    new create制作

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+  def new
+    @post = Post.new
+
+    def create
+      @post = Post.new(post_params)
+
+      # if @post.save
+      #   redirect_to root_path, notice: "投稿が作成されました。"
+      # else
+      #   render :new
+      # end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
